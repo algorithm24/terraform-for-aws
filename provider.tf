a56c9cefa02350fcca6bd5914be67f002ae99d02
duyle <18520661@gm.uit.edu.vn>
2022-11-11 15:34:50 +0700

    update for terraform cloud

--- provider.tf
+++ provider.tf
@@ -3,3 +3,12 @@ provider "aws" {
   secret_key = var.secret_key
   region     = "us-east-1"
 }
+
+terraform {
+  cloud {
+    organization = "starlight"
+    workspaces {
+      name = "terraform-for-aws"
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  type = string
+  type    = string
   default = ""
 }
 variable "secret_key" {
-  type = string
+  type    = string
   default = ""
 }
