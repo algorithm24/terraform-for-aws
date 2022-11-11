df8fb708f8faeee25ecd671f846d6cefe51dab76
duyle <18520661@gm.uit.edu.vn>
2022-11-11 11:51:01 +0700

    update for terraform cloud used

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
-locals {
-  access_key = ""
-  secret_key = ""
-}
 provider "aws" {
-  access_key = local.access_key
-  secret_key = local.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
   region     = "us-east-1"
 }
